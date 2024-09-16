4a3cb0ce5d0cb8a6d5d48cfd0d20e593e466c7f3
Noor Chhena <schhena@se14l0120-17.ad.bcit.ca>
2024-09-16 11:10:19 -0700

    class 2 work

---
+++ whatever.js
@@ -0,0 +1,17 @@
+let clothes = "coats and sweaters";
+const season = "fall";
+
+console.log("This weekend, I'm planning to go to the mall to buy some new " + clothes + " for " + season + " weather. Most of my old " + clothes + " are too worn or too small for me by now, so it felt right to clean out my closet and replace all the " + clothes + " I own this " + season + ".");
+
+clothes = "winter clothes";
+
+console.log("Other than that, though, I don't have many plans. With college starting this " + season + ", most of my energy is focused in that direction, so I'm not left with much time for myself unless it's something practical like shopping for " + clothes + ". I didn't even do much for my " + 18 + "th birthday, which happened just a week or two ago. Turning " + 18 + " seems like it should have felt like a huge turning point in my life, but to be honest, everything feels about the same.");
+
+//-write about cold weather plans and being busy with college this weekend
+//-let could be "clothes", const could be "fall"?
+//-could use age (talking about turning 18) as number
+//-date could also be number?
+
+
+//Perfecto 
+// comments from Bruno Amorim
